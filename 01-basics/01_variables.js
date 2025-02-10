5332e262fd259e0c7e79a41d3429c89a78cb5a00
Tanishq Chholak <rudrachholak@gmail.com>
2025-02-10 09:48:09 +0000

    Variables config

---
+++ 01-basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 1234 // constant value do not change
+let accountMail = "Tanishq@gmail.com"
+var accountPassword = "23143"
+accountCity = "Jaipur"
+
+console.log(accountId)
+console.table([accountId, accountMail, accountPassword, accountCity])
