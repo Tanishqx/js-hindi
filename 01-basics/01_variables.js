ae09b1b4a59a4cd5680e067005923bb64c7189f1
Tanishq Chholak <rudrachholak@gmail.com>
2025-02-10 09:57:17 +0000

    commit

--- 01-basics/01_variables.js
+++ 01-basics/01_variables.js
@@ -1,7 +1,20 @@
 const accountId = 1234 // constant value do not change
-let accountMail = "Tanishq@gmail.com"
-var accountPassword = "23143"
+let accountMail = "Tanishq@gmail.com"  // you can declare variable by 2 ways : let or var
+var accountPassword = "23143" 
 accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+// curly braces is known as scope
+/*
+Note: Prefer not to use var because of issue in block scope and functional scope
+*/
+
+accountMail = "tan@gmail.com" // variable value can be changed 
+accountPassword = "2123"
+accountCity = "Bangalore"
+
 
 console.log(accountId)
-console.table([accountId, accountMail, accountPassword, accountCity])
+console.table([accountId, accountMail, accountPassword, accountCity, accountState])
