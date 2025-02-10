54aac93d52caf90f1510629aadabd00ce775d4d4
Tanishq Chholak <rudrachholak@gmail.com>
2025-02-10 10:56:33 +0000

    full

--- 01-basics/03_conversionOperation.js
+++ 01-basics/03_conversionOperation.js
@@ -12,7 +12,7 @@ let valueInNumber = Number(score)
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -21,4 +21,26 @@ console.log(booleanIsLoggedIn);
 let schoolId = 21
 
 let stringNumber = String(schoolId)
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+// ********************** Operations **********************************
+
+let value = 4
+let negValue = -value
+// console.log(negValue);
+// console.log(3+5);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(6/2);
+// console.log(2%3);
+
+let str1 = "tanishq "
+let str2 = "how are you"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log( 1 + "2");
+console.log( "2" + 2);
+console.log( 1 + 2 + "2" ); // value of first datatype will be considered
+
+
