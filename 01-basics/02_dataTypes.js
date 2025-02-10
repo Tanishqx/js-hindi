47b7c7400cb089eeccc494fc2c12b6c04ee1fc6f
Tanishq Chholak <rudrachholak@gmail.com>
2025-02-10 10:15:15 +0000

    dataTypes

---
+++ 01-basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3+3 ) // we are using nodeJS, not browser
+console.log(3+4)
+
+// code readibility should be high
+// Datatype in javascript
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is yet to be defined
+// symbol => unique
+
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null)  // object
