bf60ff0cf04692a05fd5df7439f23cb4cd373372
Tanishq <rudrachholak@gmail.com>
2025-02-12 14:30:34 +0530

    arrays

---
+++ 02-basics/01_arrays.js
@@ -0,0 +1,43 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// console.log(myArr[0]);
+// console.log(myArr);
+
+const myHeros = ['batman', 'ironman', 'thor'];
+const myArr2 = new Array(12, 23, 34, 45, 56, 67, 78);
+// console.log(myArr2); 
+// console.log(myHeros);
+
+// myArr.push(3)
+// console.log(myArr);     
+// myArr.pop();
+// console.log(myArr);
+
+// myArr.unshift(0); // add element at the start, but it is not recommended
+// myArr.shift(); // remove element from the start, but it is not recommended
+
+// console.log(myArr.includes(5));
+// console.log(myArr.indexOf(5));
+
+// const newArr = myArr.join();
+
+// console.log(newArr);
+// console.log(typeof newArr);
+
+// Methods: Slice and Splice
+
+// console.log("A: ", myArr);
+// const myArr1 = myArr.slice(1, 4); // slice method
+// console.log(myArr1); // slice method
+// console.log("B: ", myArr);
+
+// const myn2 = myArr.splice(1,4);
+// console.log("C: ", myArr);
+// console.log(myn2); // splice method
+
+
+
+
+
+
